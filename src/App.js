7f833ea4d89538989647634b4c0c6fce075825a2
sharvariawadhane <awadhanesharvari@gmail.com>
2025-01-02 15:47:09 +0530

    Added missing files and fixed routing issues

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Button, Container, Typography } from '@mui/material';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const App = () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
+
+  return (
+    <Router>
+      <Container>
+        <Typography variant="h4" gutterBottom>
+          RTO Special Services
+        </Typography>
+        <Switch>
+          <Route exact path="/">
+            <HomePage location={location} />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
+  );
+};
+
+const HomePage = ({ location }) => {
+  return (
+    <div>
+      {location ? (
+        <>
+          <MapContainer center={[location.lat, location.lng]} zoom={13} style={{ height: '400px', width: '100%' }}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[location.lat, location.lng]}>
+              <Popup>You are here!</Popup>
+            </Marker>
+          </MapContainer>
+          <Button variant="contained" color="primary">
+            Get Status Updates
+          </Button>
+        </>
+      ) : (
+        <Typography>Loading location...</Typography>
+      )}
+    </div>
+  );
+};
+
+export default App;
